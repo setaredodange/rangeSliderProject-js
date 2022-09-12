69d1ad3f131f46245cedddde5d5233d437a989b8
somayehdodangeh <dodangeh7019@gmail.com>
2022-09-12 11:00:21 -0700

    completed

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+const rangeInput = document.querySelector('input')
+const container = document.querySelector('.container')
+
+
+rangeInput.addEventListener('change', function (event) {
+
+     // console.log(event.target.value);
+    container.style.filter = 'brightness(' + event.target.value + '%)'
+})
